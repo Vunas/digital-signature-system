362859158e6278c213791687ece9cd5780d8fa9b
Vu Hoang <vuh265@gmail.com>
2026-05-20 18:30:05 +0700

    fix error session

--- app/db/session.py
+++ app/db/session.py
@@ -1,9 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 from app.core.config import settings
 
-engine_kwargs = {"echo": False, "pool_pre_ping": True}
+engine_kwargs = {
+    "echo": False,
+    "pool_pre_ping": True,
+}
+
 if not settings.DATABASE_URL.startswith("sqlite"):
-    engine_kwargs.update({"pool_size": 10, "max_overflow": 20})
+    engine_kwargs.update(
+        {"pool_size": 10, "max_overflow": 20, "connect_args": {"statement_cache_size": 0}}
+    )
 
 engine = create_async_engine(settings.DATABASE_URL, **engine_kwargs)
 
